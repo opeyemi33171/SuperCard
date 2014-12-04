7a5a39f1e724e7c43ae31252fd6ffd9f771e19f1
opeyemi33171 <opeyemi33171@gmail.com>
2014-12-04 06:48:44 -0800

    Upload 1.0

---
+++ SuperCards/CardPack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCards
+{
+    public class CardPack
+    {
+        public string name;
+        public string numberOfCardsInPack;
+        public string percentageOfCardsMemorized;
+
+    }
+}
